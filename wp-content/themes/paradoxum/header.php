07eede3e24f3caa9077697fb57235aee3d2d6c89
Michał Biel <micha@kumulugma.pl>
2022-07-15 09:33:05 +0200

    0.0.5a - Synchronizacja projektu

--- wp-content/themes/paradoxum/header.php
+++ wp-content/themes/paradoxum/header.php
@@ -10,6 +10,7 @@
         <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
         
             <?php wp_head(); ?>
+        <!-- Favicons -->
+        <meta name="theme-color" content="#7952b3">
     </head>
-<body <?php body_class(); ?>>
-    <?php wp_body_open(); ?>
+    <body>
